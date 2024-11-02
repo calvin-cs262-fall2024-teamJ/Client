f00580c9724d3321586e4ce1339db015aa7bf8c6
will-elsen <willtelsen@gmail.com>
2024-11-02 14:07:18 -0400

    successful merge

--- journeysmith/HomeScreen.tsx
+++ journeysmith/HomeScreen.tsx
@@ -1,53 +1,81 @@
-import React from 'react';
-import { ImageBackground, View, Text, Pressable, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { ImageBackground, View, Text, Pressable, StyleSheet, Dimensions, Image } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
 
 function HomeScreen({ navigation }) {
+  
   return (
-    <ImageBackground
-      source={require('./assets/home-screen-map.jpg')}
-      style={styles.backgroundImage}
+    <View
+      style={styles.background}
     >
-      <View style={styles.overlay}>
-        <Text style={styles.welcomeText}>Welcome to Journeysmith!</Text>
-      </View>
-        <Pressable style={styles.button} onPress={() => navigation.navigate("MapList")}>
-          <Text style={styles.buttonText}>Go to Map List</Text>
-        </Pressable>
-    </ImageBackground>
+      <Image source={require('./assets/logo.jpg')} style={styles.logo}></Image>
+      <Text style={styles.welcomeText}>Welcome to Journeysmith!</Text>
+      <Pressable style={styles.button} onPress={() => navigation.navigate('MapList')}>
+        <Text style={styles.buttonText}>Get Started!</Text>
+      </Pressable>
+    </View>
   );
 }
 
 
 const styles = StyleSheet.create({
-  backgroundImage: {
-    flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
-    alignItems: 'center',
+  logo: {
+    width: 200,
+    height: 200,
   },
   overlay: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0)', // Optional: Adds a semi-transparent overlay
+  },
+  background: {
+    backgroundColor: '#1B1921',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   welcomeText: {
     fontSize: 24,
-    color: 'rgba(255, 255, 255, 1)',
+    color: '#rgba(235, 235, 200, 1)',
     fontWeight: 'bold',
-    textShadowColor: 'rgba(0, 0, 0, 1)',
+    textShadowColor: '#rgba(255, 255, 255, 0.3)',
     textShadowOffset: { width: 0, height: 2 },
     textShadowRadius: 10,
   },
   button: {
-    marginTop: 20,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    marginTop: 10,
+    marginLeft: 10,
     paddingVertical: 10,
     paddingHorizontal: 20,
-    backgroundColor: '#000',
+    backgroundColor: '#569A81',
     borderRadius: 5,
+    borderColor: 'rgba(235, 235, 200, 1)',
+    borderWidth: 3,
   },
   buttonText: {
-    color: 'white',
+    color: '#CE774A',
+    fontWeight: 'bold',
+  },
+  dropdown: {
+    position: 'absolute',
+    top: 50,
+    left: 10,
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    borderRadius: 5,
+    borderColor: 'rgba(105, 63, 27,1)',
+    borderWidth: 1,
+    zIndex: 1,
+  },
+  dropdownItem: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  dropdownItemText: {
+    color: '#000',
   },
 });
 
--- journeysmith/MapScreen.tsx
+++ journeysmith/MapScreen.tsx
@@ -1,3 +1,73 @@
+// import React from 'react';
+// import { View, Image, StyleSheet, Pressable, Text, Dimensions } from 'react-native';
+// import { useRoute, RouteProp, useNavigation, NavigationProp } from '@react-navigation/native';
+// import NotesBox from './NotesBox';
+
+// type RouteParams = {
+//     params: {
+//         imageUri: string;
+//     };
+// };
+
+// type RootStackParamList = {
+//     Home: undefined;
+//     MapList: undefined;
+//     MapScreen: { imageUri: string };
+// };
+
+// // MapScreen component
+// const MapScreen = () => {
+//     const route = useRoute<RouteProp<RouteParams, 'params'>>();
+//     const { imageUri } = route.params;
+//     const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+
+//     console.log('Received imageUri:', imageUri); // For debugging
+
+//     return (
+//         <View style={styles.container}>
+//             {imageUri ? (
+//                 <Image
+//                     source={{ uri: imageUri }}
+//                     style={styles.image}
+//                     resizeMode="contain" // Ensure the entire image fits within the screen
+//                 />
+//             ) : null}
+//             <Pressable style={styles.returnButton} onPress={() => navigation.goBack()}>
+//                 <Text style={styles.returnButtonText}>Return to map list</Text>
+//             </Pressable>
+//             <NotesBox /> {/* Allow DM to take notes on the side of the mapscreen */}
+//         </View>
+//     );
+// }
+
+// // Styles for the map component
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         backgroundColor: 'black', // Set the background color to black
+//     },
+//     //Display map image with correct dimensions as large as possible based on screen size
+//     image: {
+//         flex: 1,
+//         width: Dimensions.get('window').width,
+//         height: Dimensions.get('window').height,
+//     },
+//     returnButton: {
+//         position: 'absolute',
+//         top: 10,
+//         left: 10,
+//         padding: 10,
+//         backgroundColor: '#fff',
+//     },
+//     returnButtonText: {
+//         color: '#000',
+//     },
+// });
+
+// export default MapScreen;
+
 import React from 'react';
 import { View, Image, StyleSheet, Pressable, Text, Dimensions } from 'react-native';
 import { useRoute, RouteProp, useNavigation, NavigationProp } from '@react-navigation/native';
@@ -15,6 +85,8 @@ type RootStackParamList = {
     MapScreen: { imageUri: string };
 };
 
+const { width, height } = Dimensions.get('window');
+
 // MapScreen component
 const MapScreen = () => {
     const route = useRoute<RouteProp<RouteParams, 'params'>>();
@@ -26,16 +98,12 @@ const MapScreen = () => {
     return (
         <View style={styles.container}>
             {imageUri ? (
-                <Image
-                    source={{ uri: imageUri }}
-                    style={styles.image}
-                    resizeMode="contain" // Ensure the entire image fits within the screen
-                />
+                    <Image source={{ uri: imageUri }} style={styles.image} />
             ) : null}
             <Pressable style={styles.returnButton} onPress={() => navigation.goBack()}>
                 <Text style={styles.returnButtonText}>Return to map list</Text>
             </Pressable>
-            <NotesBox /> {/* Allow DM to take notes on the side of the mapscreen */}
+            <NotesBox />
         </View>
     );
 }
@@ -46,23 +114,36 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'black', // Set the background color to black
+        backgroundColor: '#rgba(105, 63, 27,1)',
     },
-    //Display map image with correct dimensions as large as possible based on screen size
     image: {
-        flex: 1,
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
+        width: width - 20,
+        height: height - 20,
+        resizeMode: 'contain',
+        borderColor: '#rgba(105, 63, 27,1)',
+        backgroundColor: '#rgba(105, 63, 27,1)',
+        borderRadius: 10,
     },
     returnButton: {
         position: 'absolute',
         top: 10,
-        left: 10,
+        right: 10,
         padding: 10,
-        backgroundColor: '#fff',
+        backgroundColor: '#rgba(245, 245, 220, 1)',
+        borderRadius: 10,
     },
     returnButtonText: {
         color: '#000',
+    },
+    pinButton: {
+        backgroundColor: '#rgba(245, 245, 220, 1)',
+        width: 10,
+        height: 10,
+        borderRadius: 5,
+        borderColor: '#rgba(245, 245, 220, 1)',
+    },
+    zoomableView: {
+
     },
 });
 
