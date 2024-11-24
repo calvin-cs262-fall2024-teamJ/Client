0b8c85c00f8cdc7e6f17b0d26ff6cb037f700a3f
will-elsen <willtelsen@gmail.com>
2024-11-24 12:58:48 -0500

    strange eslint error. trying to fix by brute force

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import pluginReact from "eslint-plugin-react";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  ...tseslint.configs.recommended,
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  // ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
